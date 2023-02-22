2bcde76892df1f0dc8650d066b685bac41eb5a92
Thiago da Rocha Miguel <thiago.miguel@gec.inatel.br>
2023-02-22 09:41:56 -0300

    Dart

---
+++ curso_alura_dart/bin/kako_bot.dart
@@ -0,0 +1,50 @@
+import 'dart:io';
+
+import 'questions/good_manners.dart';
+import 'questions/time_questions.dart';
+import 'timing/waiting_time.dart';
+
+void main() async {
+  String kakoBot = 'KakoBOT:\n';
+  var a = true;
+  String usuario = '';
+
+  var myStream = BotClock().kakoBotStream(1, 10);
+  var subscriber = myStream.listen((event) {
+    print('                      KakoBot está ativo a: $event segundos');
+  }, onDone: () {
+    print('KakoBot está sendo finalizado! Faça a ultima pergunta.');
+    a = false;
+  });
+
+  print('-- Iniciando o KakoBOT, aguarde..--');
+  await BotClock().clock(3);
+  print('KakoBOT:\n Oi :) \n Como posso ajudar?');
+  do {
+    usuario = stdin.readLineSync().toString();
+    print('-- Processando pergunta, aguarde..--');
+    await BotClock().clock(1);
+    if (usuario.contains('xau') ||
+        usuario.contains('Xau') ||
+        usuario.contains('Adeus') ||
+        usuario.contains('adeus')) {
+      a = false;
+      print(kakoBot + ' Até a proxima!!');
+    } else if (TimeQuestions(usuario).isThisTime()) {
+      // verificar antes, assim não fazemos toda a função sem precisar.
+      TimeQuestions(usuario).timeQuestion();
+      await BotClock().clock(1);
+    } else if (GoodManners(usuario).isThisManners()) {
+      GoodManners(usuario).goodManners();
+    } else if (false) {
+      //Basta adicionar novas perguntas aqui!
+    } else {
+      await BotClock().clock(2);
+      print(kakoBot +
+          ' Não fui treinado para responder a essa pergunta \n Desculpe :( ');
+      print(kakoBot + ' Você pode fazer outra pergunta ou dizer Adeus');
+    }
+  } while (a);
+
+  print('--Encerrando KakoBOT--');
+}
---
+++ curso_alura_dart/test/curso_alura_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:curso_alura_dart/curso_alura_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
